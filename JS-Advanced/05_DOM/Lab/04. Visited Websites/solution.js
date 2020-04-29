3818f25ad5a74728a901a38361d504432fd01625
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2020-04-29 11:17:06 +0200

    Update solution.js

--- JS-Advanced/05_DOM/Lab/04. Visited Websites/solution.js
+++ JS-Advanced/05_DOM/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,14 @@
 function solve() {
+    const links = document.getElementsByClassName('link-1');
 
-  //TODO...
+    for (let i = 0; i < links.length; i++) {
+
+        const xref = links[i].firstElementChild;
+
+        xref.addEventListener('click', function () {
+
+            const countElement = this.nextElementSibling;
+            countElement.textContent = countElement.textContent.replace(/[0-9]+/g, c => `${ parseInt(c) + 1 }`)
+        })
+    }
 }
