e276a6fe7c314fea7e80399d057027456b56cc9a
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2020-06-03 16:44:01 +0200

    Initial commit

--- JS-Advanced/05_DOM-Manipulations/Exercise/temp/03. Accordion/accordion.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-    const buttonText = document.getElementsByClassName('button')[0].firstChild;
-    buttonText.textContent = buttonText.textContent === 'More' ? 'Less' : 'More';
-
-    const extra = document.getElementById('extra');
-    extra.style.display = buttonText.textContent === 'More' ? 'none' : 'block';
-}
